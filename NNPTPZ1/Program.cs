31e670d7ad105774f3102a7198b2a0d25ec46680
Jan Rybka <st79605@upce.cz>
2025-10-14 22:38:53 +0200

    Splited main to separated functions.

--- NNPTPZ1/Program.cs
+++ NNPTPZ1/Program.cs
@@ -15,7 +15,40 @@ namespace NNPTPZ1
 
         public static Color[] colourPallete = { Color.Red, Color.Blue, Color.Green, Color.Yellow, Color.Orange, Color.Fuchsia, Color.Gold, Color.Cyan, Color.Magenta };
         public static List<ComplexNumber> roots = null;
+
         static void Main(string[] args)
+        {
+            // TODO: x y Coordinates
+            // TODO: argument parser
+            // TODO: hide poly adding coefieients
+            // TODO: 
+            Tuple<int, int> sizeInpixels;
+            Tuple<double, double> min;
+            Tuple<double, double> max;
+            string fileName;
+
+            (sizeInpixels, min, max, fileName) = ParseArgs(args);
+            // TODO: add parameters from args?
+            Bitmap bmp = new Bitmap(sizeInpixels.Item1, sizeInpixels.Item2);
+
+            Tuple<double, double> stepSize = Tuple.Create((max.Item1 - min.Item1) / sizeInpixels.Item1, (max.Item2 - min.Item2) / sizeInpixels.Item2);
+
+            roots = new List<ComplexNumber>();
+            // TODO: poly should be parameterised?
+            (Polynomial polynom, Polynomial derivatedPolynom) = InitiatePolynoms();
+            // for every pixel in image...
+            for (int y = 0; y < sizeInpixels.Item1; y++)
+            {
+                for (int x = 0; x < sizeInpixels.Item2; x++)
+                {
+                    Color color = determinePixelCollour(polynom, derivatedPolynom, min, Tuple.Create(x, y), stepSize);
+                    bmp.SetPixel(x, y, color);
+                }
+            }
+            bmp.Save(fileName ?? "../../../out.png");
+        }
+        
+        public static (Tuple<int, int> sizesInPixels, Tuple<double, double> min, Tuple<double, double> max, string fileName) ParseArgs(string[] args)
         {
             int xPixelSize = int.Parse(args[0]);
             int yPixelSize = int.Parse(args[1]);
@@ -26,15 +59,11 @@ namespace NNPTPZ1
             double ymax = double.Parse(args[5]);
 
             string output = args[6];
+            return (sizesInPixels: Tuple.Create(xPixelSize, yPixelSize), min: Tuple.Create(xmin, ymin), max: Tuple.Create(xmax, ymax), fileName: output);
+        }
 
-            // TODO: add parameters from args?
-            Bitmap bmp = new Bitmap(xPixelSize, yPixelSize);
-
-            double xstep = (xmax - xmin) / xPixelSize;
-            double ystep = (ymax - ymin) / yPixelSize;
-
-            roots = new List<ComplexNumber>();
-            // TODO: poly should be parameterised?
+        public static (Polynomial polyno, Polynomial derivated) InitiatePolynoms()
+        {
             Polynomial polynom = new Polynomial();
             polynom.Coeficients.Add(new ComplexNumber() { RealPart = 1 });
             polynom.Coeficients.Add(ComplexNumber.Zero);
@@ -42,34 +71,25 @@ namespace NNPTPZ1
             polynom.Coeficients.Add(new ComplexNumber() { RealPart = 1 });
             Polynomial derivatedPolynom = polynom.Derive();
 
-
             Console.WriteLine(polynom);
             Console.WriteLine(derivatedPolynom);
+            return (polyno: polynom, derivated: derivatedPolynom);
+        }
 
-            // for every pixel in image...
-            for (int y = 0; y < xPixelSize; y++)
-            {
-                for (int x = 0; x < yPixelSize; x++)
-                {
-                    // find "world" coordinates of pixel
-                    double argumentRealPart = xmin + x * xstep;
-                    double argumentImaginariPart = ymin + y * ystep;
-
-                    // TODO: decide if foloving part is stil necesarry or can be deleted
-                    if (argumentRealPart == 0)
-                        argumentRealPart = 0.0001;
-                    if (argumentImaginariPart == 0)
-                        argumentImaginariPart = 0.0001;
-
-                    Color color = getPixelColour(
-                        ref polynom,
-                        ref derivatedPolynom,
-                        new ComplexNumber() { RealPart = argumentRealPart, ImaginariPart = argumentImaginariPart }
-                    );
-                    bmp.SetPixel(x, y, color);
-                }
-            }
-            bmp.Save(output ?? "../../../out.png");
+        public static Color determinePixelCollour(Polynomial polynom, Polynomial derivatedPolynom, Tuple<double, double> min, Tuple<int, int> position, Tuple<double, double> stepSize)
+        {
+            double argumentRealPart = min.Item1 + position.Item1 * stepSize.Item1;
+            double argumentImaginariPart = min.Item2 + position.Item2 * stepSize.Item2;
+            // TODO: decide if foloving part is stil necesarry or can be deleted
+            if (argumentRealPart == 0)
+                argumentRealPart = 0.0001;
+            if (argumentImaginariPart == 0)
+                argumentImaginariPart = 0.0001;
+            return getPixelColour(
+                ref polynom,
+                ref derivatedPolynom,
+                new ComplexNumber() { RealPart = argumentRealPart, ImaginariPart = argumentImaginariPart }
+            );
         }
         public static Color getPixelColour(ref Polynomial polynom, ref Polynomial derivatedPolynom, ComplexNumber argument)
         {
