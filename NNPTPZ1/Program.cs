9e6b1565f2557c2fff82c968f7deb9503e528c21
Jiří Vlk <vlkjir00@gmail.com>
2025-10-18 03:54:41 +0200

    odstranění mrtvého kódu, drobný úklid

--- NNPTPZ1/Program.cs
+++ NNPTPZ1/Program.cs
@@ -32,6 +32,7 @@ namespace NNPTPZ1
             const int MaxIterations = 30;
             const double ConvergenceThreshold = 0.5;
             const float Epsilon = 0.0001f;
+            const double RootTolerance = 0.01;
 
             var bmp = new Bitmap(width, height);
 
@@ -39,27 +40,22 @@ namespace NNPTPZ1
             double ystep = (ymax - ymin) / height;
 
             List<Cplx> koreny = new List<Cplx>();
+
             // TODO: poly should be parameterised?
             Poly p = new Poly();
             p.Coe.Add(new Cplx() { Re = 1 });
             p.Coe.Add(Cplx.Zero);
             p.Coe.Add(Cplx.Zero);
-            //p.Coe.Add(Cplx.Zero);
             p.Coe.Add(new Cplx() { Re = 1 });
-            Poly ptmp = p;
             Poly pd = p.Derive();
 
-            Console.WriteLine(p);
-            Console.WriteLine(pd);
+
 
             var clrs = new Color[]
             {
                 Color.Red, Color.Blue, Color.Green, Color.Yellow, Color.Orange, Color.Fuchsia, Color.Gold, Color.Cyan, Color.Magenta
             };
 
-            var maxid = 0;
-
-            // TODO: cleanup!!!
             // for every pixel in image...
             for (int i = 0; i < width; i++)
             {
@@ -80,8 +76,6 @@ namespace NNPTPZ1
                     if (ox.Imaginari == 0)
                         ox.Imaginari = Epsilon;
 
-                    //Console.WriteLine(ox);
-
                     // find solution of equation using newton's iteration
                     float it = 0;
                     for (int q = 0; q< MaxIterations; q++)
@@ -89,7 +83,6 @@ namespace NNPTPZ1
                         var diff = p.Eval(ox).Divide(pd.Eval(ox));
                         ox = ox.Subtract(diff);
 
-                        //Console.WriteLine($"{q} {ox} -({diff})");
                         if (Math.Pow(diff.Re, 2) + Math.Pow(diff.Imaginari, 2) >= ConvergenceThreshold)
                         {
                             q--;
@@ -97,14 +90,12 @@ namespace NNPTPZ1
                         it++;
                     }
 
-                    //Console.ReadKey();
-
                     // find solution root number
                     var known = false;
                     var id = 0;
                     for (int w = 0; w <koreny.Count;w++)
                     {
-                        if (Math.Pow(ox.Re- koreny[w].Re, 2) + Math.Pow(ox.Imaginari - koreny[w].Imaginari, 2) <= 0.01)
+                        if (Math.Pow(ox.Re- koreny[w].Re, 2) + Math.Pow(ox.Imaginari - koreny[w].Imaginari, 2) <= RootTolerance)
                         {
                             known = true;
                             id = w;
@@ -114,34 +105,20 @@ namespace NNPTPZ1
                     {
                         koreny.Add(ox);
                         id = koreny.Count;
-                        maxid = id + 1; 
                     }
 
                     // colorize pixel according to root number
-                    //int vv = id;
-                    //int vv = id * 50 + (int)it*5;
-                    var vv = clrs[id % clrs.Length];
-                    vv = Color.FromArgb(vv.R, vv.G, vv.B);
-                    vv = Color.FromArgb(Math.Min(Math.Max(0, vv.R-(int)it*2), 255), Math.Min(Math.Max(0, vv.G - (int)it*2), 255), Math.Min(Math.Max(0, vv.B - (int)it*2), 255));
-                    //vv = Math.Min(Math.Max(0, vv), 255);
-                    bmp.SetPixel(j, i, vv);
-                    //bmp.SetPixel(j, i, Color.FromArgb(vv, vv, vv));
+                    var baseColor = clrs[id % clrs.Length];
+                    var shaded = Color.FromArgb(
+                        Math.Min(Math.Max(0, baseColor.R - (int)it * 2), 255),
+                        Math.Min(Math.Max(0, baseColor.G - (int)it * 2), 255),
+                        Math.Min(Math.Max(0, baseColor.B - (int)it * 2), 255)
+                        );
+                    bmp.SetPixel(j, i, shaded);
                 }
             }
 
-            // TODO: delete I suppose...
-            //for (int i = 0; i < 300; i++)
-            //{
-            //    for (int j = 0; j < 300; j++)
-            //    {
-            //        Color c = bmp.GetPixel(j, i);
-            //        int nv = (int)Math.Floor(c.R * (255.0 / maxid));
-            //        bmp.SetPixel(j, i, Color.FromArgb(nv, nv, nv));
-            //    }
-            //}
-
                     bmp.Save(output ?? "../../../out.png");
-            //Console.ReadKey();
         }
     }
 
